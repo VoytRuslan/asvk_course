dab4c232660ecb1dc5c97f471a16014d74bc55e2
VoyRUS <vojt.ruslan@gmail.com>
2025-03-02 14:18:07 +0300

    Add gameplay function

--- 03_MergeRequirements/bullcows.py
+++ 03_MergeRequirements/bullcows.py
@@ -1,3 +1,6 @@
+import random
+
+
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
     '''
     Функция сравнивает догадку игрока с загаданным словом и возвращает количество быков и коров
@@ -23,4 +26,28 @@ def bullscows(guess: str, secret: str) -> tuple[int, int]:
 
     return bulls, cows
 
-print(bullscows("ропот", "полип"))  # -> (1, 1)
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    '''
+    Функция обеспечивает геймплей игры "Быки и коровы"
+    Задумывает случайное слово из списка слов words
+    Спрашивает у пользователя слово с помощью функции ask("Введите слово: ", words)
+    Выводит пользователю результат с помощью функции inform("Быки: {}, Коровы: {}", b, c)
+    Если слово не отгадано, снова спрашивает слово у пользователя
+
+    :param ask: callable - функция, спрашивает у пользователя слово
+    :param inform: callable - функция, выводит результат игры
+    :param words: list[str] - список слов для загадывания
+    :return: int - количество попыток
+    '''
+
+    secret = random.choice(words)
+    attempts = 0
+    while True:
+        guess = ask("Введите слово: ", words)
+        attempts += 1
+        bulls, cows = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+        if bulls == len(secret):
+            break
+    return attempts
